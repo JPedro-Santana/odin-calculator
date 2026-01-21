6feae5158965c7de2e48b51b46ae6589cffd278f
João Pedro Santana <joao.pedrosousa5@gmail.com>
2026-01-21 18:16:51 -0300

    Added simple logic, structure and style

---
+++ script.js
@@ -0,0 +1,24 @@
+let firstOperand;
+let operator;
+let secondOperand;
+
+function add(a, b) {
+    return a + b;
+}
+
+function subtract(a, b) {
+    return a - b;
+}
+
+function multiply(a, b) {
+    return a * b;
+}
+
+function divide(a, b) {
+    return a / b;
+}
+
+function operate(operator, firstNum, secondNum){
+
+}
+
