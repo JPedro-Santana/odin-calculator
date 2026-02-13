c699ce5bf667245935ec20c49f5a2f23d0039923
João Pedro Santana <joao.pedrosousa5@gmail.com>
2026-02-13 17:31:11 -0300

    script and style adjustment

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
-let firstOperand;
-let operator;
-let secondOperand;
+let firstOperand = '';
+let operator = '';
+let secondOperand = '';
 
 function add(a, b) {
     return a + b;
@@ -18,7 +18,32 @@ function divide(a, b) {
     return a / b;
 }
 
-function operate(operator, firstNum, secondNum){
+/*Create a new function operate that takes an operator and two numbers
+ and then calls one of the above functions on the numbers.*/
+function operate(operator, firstOperand, secondOperand){
+    if (operator == '+'){
+        add(firstOperand,secondOperand)
+    }
+    else if (operator == '-'){
+        subtract(firstOperand,secondOperand)
+    }
 
 }
 
+/*  Create the functions that update one of your number variables when the calculator’s digit buttons are clicked. 
+Your calculator’s display should also update to reflect the value of that number variable.
+*/ 
+let display = document.querySelector('#display')
+
+function updateValue(value){
+
+
+}
+
+
+function clearDisplay(){
+    firstOperand = '';
+    operator = '';
+    secondOperand = '';
+    display.textContent = '0';
+}
